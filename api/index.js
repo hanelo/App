2e24d0342764267999230cdddeedf1e4ccec8ece
Hanae Tafza <89151879+hanelo@users.noreply.github.com>
2025-06-13 00:11:03 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,57 +1,54 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const serverless = require('serverless-http');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/interface.html'));
+  res.render('interface');
 });
 
 app.post('/simulate', (req, res) => {
-    const p = parseInt(req.body.passengers);
-    const t = parseFloat(req.body.processing_time);
-    const r = parseInt(req.body.num_resources);
-    const pos = parseInt(req.body.chosen_position);
+  const { passengers, processing_time, num_resources, chosen_position } = req.body;
+  const np = parseInt(passengers);
+  const pt = parseFloat(processing_time);
+  const nr = parseInt(num_resources);
+  const cp = parseInt(chosen_position);
 
-    if (p <= 0 || t <= 0 || r <= 0 || pos <= 0 || pos > p) {
-        return res.sendFile(path.join(__dirname, '../views/interface.html'));
-    }
+  const waiting_time = 0;
+  const processing = pt;
+  const total_time = waiting_time + processing;
 
-    const processing_time = t;
-    const waiting_time = 0;
-    const total_time = waiting_time + processing_time;
-    const chosen_waiting_time = pos === 1 ? 0 : (t / p) * (pos - 1);
-    const chosen_total_time = chosen_waiting_time + t;
-    const total_last = (p * t) / r;
-    const wait_last = total_last - t;
-    const wait_middle = wait_last / 2;
-    const total_middle = wait_middle + t;
-    const chosen_pos_wait = (t / r) * (pos - 1);
-    const chosen_pos_total = chosen_pos_wait + t;
-    const capacity = (r * 60) / t;
+  const cp_wait = cp === 1 ? 0 : (pt / np) * (cp - 1);
+  const cp_total = cp_wait + pt;
 
-    res.render('result', {
-        waiting_time,
-        processing_time,
-        total_time,
-        first_passenger_processing_time: t,
-        chosen_position_waiting_time: chosen_waiting_time,
-        chosen_position_processing_time: t,
-        chosen_position_total_time: chosen_total_time,
-        last_passenger_waiting_time: wait_last,
-        total_time_last_passenger: total_last,
-        passenger_middle_waiting_time: wait_middle,
-        total_time_middle_passenger: total_middle,
-        passenger_chosenposition_waiting_time: chosen_pos_wait,
-        passenger_chosenposition_total_time: chosen_pos_total,
-        capacity
-    });
+  const total_last = (np * pt) / nr;
+  const last_wait = total_last - pt;
+  const mid_wait = last_wait / 2;
+  const mid_total = mid_wait + pt;
+  const cp_waiting_time = (pt / nr) * (cp - 1);
+  const cp_total_time = cp_waiting_time + pt;
+  const capacity = (nr * 60) / pt;
+
+  res.render('result', {
+    waiting_time,
+    processing_time: processing,
+    total_time,
+    chosen_position_waiting_time: cp_wait,
+    chosen_position_total_time: cp_total,
+    last_passenger_waiting_time: last_wait,
+    total_time_last_passenger: total_last,
+    passenger_middle_waiting_time: mid_wait,
+    total_time_middle_passenger: mid_total,
+    passenger_chosenposition_waiting_time: cp_waiting_time,
+    passenger_chosenposition_total_time: cp_total_time,
+    capacity
+  });
 });
 
 module.exports = app;
-module.exports.handler = serverless(app);
